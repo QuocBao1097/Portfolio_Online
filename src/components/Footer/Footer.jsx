d977acda096e818e17a5ae6da32a2095bd0f9715
QuocBao1097 <20521097@gm.uit.edu.vn>
2023-06-09 06:56:34 +0700

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = () => {
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
         <span>baodq.dev@gmail.com</span>
+        
         <div className="f-icons">
           <Insta color="white" size={"3rem"} />
           <Facebook color="white" size={"3rem"} />
